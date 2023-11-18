46508a20d3655bb73948654e6e9ce8bf4f8f071d
Cristian Ovadiuc <covadiuc@uw.edu>
2023-11-17 21:55:10 -0800

    fix get_mave_scores

--- analysis/get_mave_scores.py
+++ analysis/get_mave_scores.py
@@ -1,52 +1,51 @@
 import pandas as pd
 import json
 import glob
-import os
+from pathlib import Path
+import argparse
 
-# Corrected the directory path to include a slash at the end
-# directory_path = '<PATH>'
+def process_json_files(directory_path):
+    directory_path = Path(directory_path)
+    json_files = directory_path.glob('*.json')
+    dataframes = []
 
-# Use os.path.dirname(os.path.abspath(__file__)) to get the current directory of the script
-directory_path = os.path.dirname(os.path.abspath(__file__)) + '/'
+    for file_path in json_files:
+        try:
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+                data = [data] if isinstance(data, dict) else data
+                df = pd.json_normalize(data)
 
+                gene_name = df.get('targetGene.name').iloc[0] if 'targetGene.name' in df.columns else None
 
+                if 'mapped_scores' in df.columns and not df['mapped_scores'].isnull().all():
+                    for scores in df['mapped_scores'].dropna():
+                        extracted_data = [{
+                            'gene_name': gene_name,
+                            'start_value': item.get('post_mapped', {}).get('variation', {}).get('location', {}).get('interval', {}).get('start', {}).get('value'),
+                            'end_value': item.get('post_mapped', {}).get('variation', {}).get('location', {}).get('interval', {}).get('end', {}).get('value'),
+                            'Ref': item.get('post_mapped', {}).get('vrs_ref_allele_seq'),
+                            'Alt': item.get('post_mapped', {}).get('variation', {}).get('state', {}).get('sequence'),
+                            'Functional score': item.get('score')
+                        } for item in scores if item.get('post_mapped')]
 
-# Using glob.glob to match the JSON file extension
-json_files = glob.glob(directory_path + '*.json')
+                        if extracted_data:
+                            df_extracted = pd.DataFrame(extracted_data)
+                            dataframes.append(df_extracted)
+        except Exception as e:
+            print(f"Error processing file {file_path}: {e}")
 
-dataframes = []
+    if dataframes:
+        final_df = pd.concat(dataframes, ignore_index=True)
+        final_csv_path = directory_path / 'mave_final_dataframe.csv'
+        final_df.to_csv(final_csv_path, index=False)
+        print(f"File successfully created at {final_csv_path}")
 
-for file_path in json_files:
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-        data = [data] if isinstance(data, dict) else data
-        df = pd.json_normalize(data)
+def main():
+    parser = argparse.ArgumentParser(description='Process some json files.')
+    parser.add_argument('directory_path', type=str, help='The directory containing the json files')
+    args = parser.parse_args()
+    process_json_files(args.directory_path)
 
-        gene_name = df['targetGene.name'].iloc[0] if 'targetGene.name' in df.columns else None
-
-        # Check if 'mapped_scores' is in the DataFrame and it's not empty
-        if 'mapped_scores' in df.columns and not df['mapped_scores'].isnull().all():
-            # Extracting data from 'mapped_scores'
-            for scores in df['mapped_scores'].dropna():
-                extracted_data = [{
-                    'gene_name': gene_name,
-                    'start_value': item.get('post_mapped', {}).get('variation', {}).get('location', {}).get('interval', {}).get('start', {}).get('value'),
-                    'end_value': item.get('post_mapped', {}).get('variation', {}).get('location', {}).get('interval', {}).get('end', {}).get('value'),
-                    'Ref': item.get('post_mapped', {}).get('vrs_ref_allele_seq'),
-                    'Alt': item.get('post_mapped', {}).get('variation', {}).get('state', {}).get('sequence'),
-                    'Functional score': item.get('score')
-                } for item in scores if item.get('post_mapped')]
-
-                # Only append if extracted_data is not empty
-                if extracted_data:
-                    df_extracted = pd.DataFrame(extracted_data)
-                    dataframes.append(df_extracted)
-
-# Concatenate all the DataFrames into one if dataframes is not empty
-if dataframes:
-    final_df = pd.concat(dataframes, ignore_index=True)
-    # Export the final DataFrame to a CSV file
-    final_csv_path = directory_path + 'mave_final_dataframe.csv'
-    final_df.to_csv(final_csv_path, index=False)
-else:
-    print("No dataframes were created, please check your JSON structure or 'mapped_scores' content.")
+if __name__ == "__main__":
+    main()
