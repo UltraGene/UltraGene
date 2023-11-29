f856e8ee51a9d5ce96d60d1a04e11432862abd22
Cristian Ovadiuc <covadiuc@uw.edu>
2023-11-28 18:43:05 -0800

    moved tests

---
+++ tests/test_snp_processor.py
@@ -0,0 +1,20 @@
+import unittest
+
+from ultragene.snp_processor import validate_and_load_snp_data  
+
+class TestSNPProcessor(unittest.TestCase):
+    
+    def test_smoke_valid_input(self):
+        pass
+
+    def test_one_shot_invalid_allele(self):
+       pass
+
+    def test_one_shot_empty_file(self):
+       pass
+
+    def test_edge_all_invalid_data(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
