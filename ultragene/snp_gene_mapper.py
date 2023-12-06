87e8a9c2ce4f92d4b99ac5bc702d9ebb20b973ba
Cristian Ovadiuc <covadiuc@uw.edu>
2023-12-06 06:53:47 -0800

    add SNP to GENE

--- ultragene/snp_gene_mapper.py
+++ ultragene/snp_gene_mapper.py
@@ -1,37 +1,67 @@
 import requests
+import logging
 
+# Constants
 API_BASE_URL = "https://clinicaltables.nlm.nih.gov/api/snps/v3/search"
 
-def fetch_gene_name(snp_id):
+def is_valid_snp_id(snp_id):
     """
-    Fetch the gene name associated with the given SNP ID from 23andME raw text file.
+    Validate the SNP ID format.
 
-    :param snp_id: The SNP ID (e.g., rs116587930).
-    :return: The gene name associated with the SNP ID or an error message.
+    :param snp_id: The SNP ID to validate.
+    :return: Boolean indicating whether the SNP ID is valid.
+    """
+    return snp_id.startswith('rs') and snp_id[2:].isdigit()
+
+def make_api_request(snp_id, assembly='38'):
+    """
+    Make an API request to fetch SNP data.
+
+    :param snp_id: The SNP ID.
+    :param assembly: The genome assembly version (e.g., '37' or '38').
+    :return: The response JSON or None if an error occurs.
     """
     try:
-        # Constructing the API URL with required parameters
         params = {
             "terms": snp_id,
-            "ef": "38.gene",  # Fetching gene name for GRCh38 assembly
-            "cf": "rsNum"     # Reference SNP accession number
+            "ef": f"{assembly}.gene",
+            "cf": "rsNum"
         }
         response = requests.get(API_BASE_URL, params=params)
 
-        # Check if the response is successful
         if response.status_code != 200:
-            return "Error: Unable to fetch data from the API."
+            logging.error(f"API call failed with status code: {response.status_code}")
+            return None
 
-        data = response.json()
-        
-        # Extracting gene name
-        gene_names = data[3][0][-1] if data[3] else "No gene name found for the given SNP ID."
-
-        return gene_names
+        return response.json()
+    except requests.RequestException as req_err:
+        logging.error(f"Request error: {req_err}")
     except Exception as e:
-        return f"An error occurred: {e}"
+        logging.error(f"An unexpected error occurred: {e}")
 
-# # Example usage
-# snp_id = "rs116587930"  # Example SNP ID
+    return None
+
+def fetch_gene_name(snp_id, assembly='38'):
+    """
+    Fetch the gene name associated with the given SNP ID.
+
+    :param snp_id: The SNP ID (e.g., rs116587930).
+    :param assembly: The genome assembly version (e.g., '37' or '38').
+    :return: The gene name associated with the SNP ID or an error message.
+    """
+    if not is_valid_snp_id(snp_id):
+        return "Invalid SNP ID format."
+
+    data = make_api_request(snp_id, assembly)
+
+    if data is None:
+        return "Error: Unable to fetch data from the API."
+
+    # Extracting gene name
+    gene_name = data[3][0][-1] if data[3] else "No gene name found for the given SNP ID."
+    return gene_name
+
+
+# snp_id = "rs122232"  # Example SNP ID
 # gene_name = fetch_gene_name(snp_id)
-# print(f"Gene name for SNP ID {snp_id}: {gene_name}")
+# print(gene_name)
